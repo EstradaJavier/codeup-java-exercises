ba78c1ffd8d3e2efe15224356b059cf113d1261d
javier estrada <estrada.javier.2022@gmail.com>
2021-03-08 20:24:11 -0600

    I am so finished wiht Bob

--- src/Bob.java
+++ src/Bob.java
@@ -1,38 +1,36 @@
-import java.text.DecimalFormat;
-import java.util.Date;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Bob {
     public static void main(String[] args) {
 
-//      Bob is a lackadaisical teenager. In conversation, his responses are very limited.
-//        Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
-//            He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
-//            He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+//        Create a class Bob for the following exercise with a main method.
+//        Bob is a lackadaisical
+//        teenager. In conversation, his responses are very limited.
+//            Bob answers 'Sure.' if you ask him a question.
+//            He answers 'Whoa, chill out!' if you yell at him.
+//            He says 'Fine. Be that way!' if you address him without actually
+//            saying anything.
 //            He answers 'Whatever.' to anything else.
-//      Write the Java code necessary so that a user of your command line application can have a
-//      conversation with Bob.
 
+        Scanner scanner = new Scanner(System.in);       // First we declare a variable
+                                                        // of type Scanner, and assign
+                                                        // to it a new Scanner object.
 
-        System.out.println("Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)");
+        System.out.print("What will you say to Bob? "); // Prints the question "out" to the terminal.
+        String userResponse = scanner.nextLine().trim();// Takes in the user response and removes the
+                                                        // white spaces.
+        System.out.println(userResponse);               // Prints out the typed response and the answer
+                                                        // base on an (if / else if / else) statement.
 
-        System.out.println("He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)");
-
-        System.out.println("He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)");
-
-        System.out.println("He answers 'Whatever.' to anything else.");
-
-
-            Scanner scan = new Scanner(System.in); //Creates a new scanner
-            System.out.println("Who is on the 5 dollar bill?"); //Asks question
-            String input = scan.nextLine(); //Waits for input
-            if (input.equalsIgnoreCase("Lincoln")) { //If the input is Lincoln (or any case variant of Lincoln)
-                System.out.println("Correct!");
-            }
-            else { //If the input is anything else
-                System.out.println("Incorrect!");
-            }
+        if (userResponse.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userResponse.endsWith("?")) {
+            System.out.println("Sorry, what was the question?");
+        } else if (userResponse.equals("")) {
+            System.out.println("No answer... OK, fine. Be that way!");
+        } else {
+            System.out.println("For real!!!");
+        }
 
 
     }
