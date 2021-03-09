0a6ae0f933609d2654503cee35f97f5439b1ba75
javier estrada <estrada.javier.2022@gmail.com>
2021-03-08 21:14:20 -0600

    done for tonight

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+
+
+//        // =========== IO EXERCISE =========== //
+          // Example for our curriculum //
+
+//        Scanner scanner = new Scanner(System.in); // First we declare a variable
+//                                                  // of type Scanner, and assign
+//                                                  // to it a new Scanner object.
+//
+//        System.out.print("Enter something: ");    // Next, prompt the user for input.
+//
+//        String userInput = scanner.next();        // obtain the value the user entered
+//                                                  // as a String, and store it
+//                                                  // in the variable userInput.
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+//
+//        // The Scanner Class
+//
+            // Prompt a user to enter a integer and store
+            // that value in an int variable using the
+            // nextInt method.
+
+        Scanner sc = new Scanner(System.in); // Declaring the Scanner and assign it to "sc".
+        sc.useDelimiter("-");                //
+        String first = sc.next();             // Obtaining value frm user input and assigning to first.
+        String second = sc.next();           // Obtaining value frm user input and assigning to second.
+        String third = sc.next();            // Obtaining value frm user input and assigning to third.
+
+        System.out.println(first + second + third); // Printing to the terminal.
+
+            System.out.print("Please enter an integer: ");
+            int userInt = sc.nextInt();
+            System.out.println(userInt);
+//
+//        // What happens if you input something that is not an integer
+////             input mismatch exception
+//
+//        // Prompt a user to enter 3 words and store each of them in a separate variable, then display them back, each on a newline.
+//             System.out.print("Enter 3 words: ");
+//             String word1 = sc.next();
+//             String word2 = sc.next();
+//             String word3 = sc.next();
+//             System.out.println(word1);
+//             System.out.println(word2);
+//             System.out.println(word3);
+
+//        // What happens if you enter less than 3 words?
+////             It won't let you continue with out finishing the inputs
+//
+//        //  What happens if you enter more than 3 words?
+////             It will take only the first three inputs
+//
+//        // Prompt a user to enter a sentence, then store that sentence in a String variable using the .next method, then display that sentence back to the user.
+//             System.out.println("Please enter a sentence:");
+//             String sentence = sc.next();
+//             System.out.println(sentence);
+
+//        // do you capture all of the words?
+//            // no
+//
+//        // Rewrite the above example using the .nextLine method.
+//
+//            System.out.println("Please enter a sentence:");
+//            String sentence = sc.nextLine();
+//            System.out.println(sentence);
+//
+//        // Calculate the perimeter and area of Codeup's classrooms. Prompt the user to enter values of length and width of the class. Use the .nextLine method to get user input and cast the resulting string to a numeric type.
+//
+//            System.out.println("Enter the length: ");
+//            String lengthInput = sc.nextLine();
+//            System.out.println("Enter the width: ");
+//            String widthInput = sc.nextLine();
+
+//
+//            double length = Double.parseDouble(lengthInput);
+//            double width = Double.parseDouble(widthInput);
+//            System.out.println("The area is: " + (length * width) + ".");
+//            System.out.println("The perimeter is: " + (length * 2 + width * 2) + ".");
+//
+//
+//        // Assume that the rooms are perfect rectangles.
+//
+//
+//
+//        // Assume that the user will enter valid numeric data for length and width.
+//        // Display the area and perimeter of that classroom.
+//
+//        // area = length x width
+//        // perimeter = 2 x length + 2 x width
+//
+//        // Bonus
+//
+//        // Accept decimal entries
+//        // Calculate the volume of the rooms
+    }
+}
