d99780a46363be907259d1f24e1ecf096552ec9e
javier estrada <estrada.javier.2022@gmail.com>
2021-03-11 09:15:38 -0600

    starting person project

--- src/Dish/Dish.java
+++
@@ -1,2 +0,0 @@
-package Dish;public class Dish {
-}
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Person {
+
+public class Person {
+    private static void main(String[] args) {
+
+//        class Person {
+//
+//            // instance variables go here
+//            public String firstName;
+//            public String lastName;
+//
+//            // creating an instance method
+//            public String sayHello() {
+//                return lastName + ", " + firstName + " says Hello";
+//            }
+//
+//        }
+
+        public String getName(){
+            //TODO: return the person's name
+            return lastName + ", " + firstName + " says Hello";
+
+        }
+
+        public void setName(String name){
+            //TODO: change the name property to the passed value
+
+        }
+        public void sayHello(){
+            //TODO: print a message to the console using the person's name
+
+        }
+
+
+    }
 }
