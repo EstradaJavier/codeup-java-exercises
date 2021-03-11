fefc82e9eb5fc6c9a321342e25e414b0d80e7304
javier estrada <estrada.javier.2022@gmail.com>
2021-03-11 09:16:57 -0600

    still using my lecture to learn

--- src/OopLecture.java
+++ src/OopLecture.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class OopLecture {
+public class OopLecture {
+    public static void main(String[] args) {
+
+        class Person {
+            public String firstName;
+            public String lastName;
+
+            public String sayHello() {
+                return String.format("Hello from %s %s!", firstName, lastName);
+            }
+
+            public static void main(String[] args) {
+                Person rick = new Person();
+                rick.firstName = "Rick";
+                rick.lastName = "Sanchez";
+                System.out.println(rick.sayHello());
+                // prints "Hello from Rick Sanchez!"
+            }
+        }
+    }
 }
