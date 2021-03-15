ae735be00689e943f675a28a101b76ce26ceb35a
javier estrada <estrada.javier.2022@gmail.com>
2021-03-14 21:17:35 -0500

    saving Imput file

---
+++ src/PPnGelly/KitchenTool.java
@@ -0,0 +1,2 @@
+package PPnGelly;public class KitchenTool {
+}
---
+++ src/util/Input.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+
+//    private String name;
+//
+//        private Scanner scanner;
+//
+//        public Input(Scanner scanner) {}
+//        this.scanner = scanner;
+
+
+
+    // // these methods will use our the Scanner method to get
+       // the user's input.
+
+//    String getString()
+
+//    boolean yesNo()
+
+//    int getInt(int min, int max)
+
+//    int getInt()
+
+//    double getDouble(double min, double max)
+
+//    double getDouble()
+}
