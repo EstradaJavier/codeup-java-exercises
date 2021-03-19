34b63021af085c1ad5b9142c055a4a60bd65f3e8
javier estrada <estrada.javier.2022@gmail.com>
2021-03-19 08:41:33 -0500

    adding tested work to my scratch pad

--- src/SleepEatCodeRepeat/ScratchPad.java
+++ src/SleepEatCodeRepeat/ScratchPad.java
@@ -1,2 +1,13 @@
-package SleepEatCodeRepeat;public class ScratchPad {
+package SleepEatCodeRepeat;
+
+public class ScratchPad {
+
+
+    int[] numbers = {1, 2, 3};
+System.out.println(numbers[999]);
+
+
+    public static void main(String[] args) {
+
+    }
 }
