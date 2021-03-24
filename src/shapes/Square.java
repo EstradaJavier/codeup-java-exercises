4fb38eb8ba2958a09ccad42fa225e9b105a3619b
javier estrada <estrada.javier.2022@gmail.com>
2021-03-24 10:10:06 -0500

    saving shapes/square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,29 +1,9 @@
-package shapes;
-
-
-public class Square extends Rectangle {
-
-    // TODO: Create a class named Square, also inside of
-    //  shapes, that extends Rectangle.
-        // COMPLETED!!!
-
-    // TODO: Square should define a constructor that
-    //  accepts one argument, side, and calls the parent's
-    //  constructor to set both the length and width to
-    //  the value of side.
-
-    // CONSTURCTOR // Calls parent constructor...
-    public Square(double side) {
-        super(side, side); // Rectable
-
-        public double getArea() {
-            return Math.pow(side, 2);
-        }
-
-        public double getPerimeter() {
-            return super.length * 4;
-        }
-
-    }
-
-}
+//package shapes;
+//
+//class Square extends Quadrilareral {
+//    private double salary;
+//    public void doWork() {
+//        System.out.println("Work, work...");
+//    }
+//}
+//
